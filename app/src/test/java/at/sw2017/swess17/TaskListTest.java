4323e3bab4bb3762a6b79efb4602639a9bc4c397
Jakob Gürtl <guertl@student.tugraz.at>
2017-05-01 12:46:24 +0200

    [JG, GO] add tasklist tests

---
+++ app/src/test/java/at/sw2017/swess17/TaskListTest.java
@@ -0,0 +1,43 @@
+package at.sw2017.swess17;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import at.sw2017.swess17.application.Task;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class TaskListTest {
+
+    @Test
+    public void taskCreation_isCorrect() throws Exception {
+        Date date = new Date();
+        Task task = new Task("Task1", "Only a task", date);
+
+        assertEquals(task.getName(),"Task1");
+        assertEquals(task.getText_description(),"Only a task");
+        assertEquals(task.getDate(),date);
+    }
+
+    public void taskList_isCorrect() throws Exception {
+        Date date = new Date();
+        Task task1 = new Task("Task1", "Only a task", date);
+        Task task2 = new Task("Task2", "Only a task", date);
+        Task task3 = new Task("Task3", "Only a task", date);
+
+        List<Task> taskList = new ArrayList<>();
+        taskList.add(task1);
+        taskList.add(task2);
+        taskList.add(task3);
+
+        assertEquals(taskList.size(),3);
+    }
+}
